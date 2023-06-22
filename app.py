82973bd09ebff367c3b69719e311e6de7642fadd
ryanmiller07 <ryan.jeffrey.miller@gmail.com>
2023-06-22 19:21:55 -0400

    code calendar

--- app.py
+++ app.py
@@ -1,4 +1,18 @@
+import datetime
+from datetime import date
 import streamlit as st
-
+# st.slider("hello",1,100,50)
 st.title("Countdown timer")
+print(date.today())
 st.write("enter a date")
+d = st.date_input(
+    "Enter an upcoming date",
+    datetime.date(int(date.today().strftime('%Y')), int(date.today().strftime('%m')),int(date.today().strftime('%d')))
+)
+
+# "with" notation
+with st.sidebar:
+    st.title("Countdown")
+
+
+
