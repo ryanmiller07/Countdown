3796434ee6d3717fea8e05be6795bf1cefe55fc9
ryan <ryan1234@gmail.com>
2023-06-29 19:09:48 -0400

    new update

--- app.py
+++ app.py
@@ -9,10 +9,25 @@ d = st.date_input(
     "Enter an upcoming date",
     datetime.date(int(date.today().strftime('%Y')), int(date.today().strftime('%m')),int(date.today().strftime('%d')))
 )
-
+print(d)
 # "with" notation
 with st.sidebar:
     st.title("Countdown")
 
+future_date = d - (date.today())
+days = future_date.days
 
+#years = ((d - (date.today())) / 365 )
+#print(years)
 
+##months = ((d - (date.today())) / 12 )
+##print(months)
+years ,days = divmod(days, 365)
+months, days = divmod(days, 30.44)
+
+print(years)
+print(months)
+print(days)
+
+st.title("Amount of years, months, and days")
+st.header(f"{years} : {months} : {days}")
