b4facae6de7254ce7c3a2e5b8290eae1a90d5877
ryanmiller07 <137454553+ryanmiller07@users.noreply.github.com>
2023-07-06 19:21:39 -0400

    Update app.py
    new update 07/06

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
-import datetime
+from datetime import datetime
+import datetime as dt
 from datetime import date
 import streamlit as st
 # st.slider("hello",1,100,50)
-st.title("Countdown timer")
-print(date.today())
-st.write("enter a date")
-d = st.date_input(
+col1,col2 = st.columns(2)
+with col1:
+    st.title("Countdown timer")
+    #print(date.today())
+    d = st.date_input(
     "Enter an upcoming date",
-    datetime.date(int(date.today().strftime('%Y')), int(date.today().strftime('%m')),int(date.today().strftime('%d')))
+    dt.date(int(date.today().strftime('%Y')), int(date.today().strftime('%m')),int(date.today().strftime('%d')))
 )
-print(d)
+with col2:
+    st.title("")
+    st.title("")
+    st.title("")
+    t = st.time_input('Enter a time of the upcoming date.')
+    print(t)
+#print(d)
 # "with" notation
 with st.sidebar:
     st.title("Countdown")
@@ -25,9 +33,9 @@ days = future_date.days
 years ,days = divmod(days, 365)
 months, days = divmod(days, 30.44)
 
-print(years)
-print(months)
-print(days)
+#print(years)
+#print(months)
+#print(days)
 
 st.title("Amount of years, months, and days")
 st.header(f"{years} : {months} : {days}")
