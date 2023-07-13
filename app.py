de8c965bc45da7ae1ae77b60df944fa35313eb11
ryan <ryan1234@gmail.com>
2023-07-13 19:08:49 -0400

    new futere time updated

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from datetime import datetime
 import datetime as dt
-from datetime import date
+from datetime import date, timedelta
 import streamlit as st
+with st.sidebar:
+    st.title("Countdown")
 # st.slider("hello",1,100,50)
 col1,col2 = st.columns(2)
 with col1:
@@ -15,13 +17,21 @@ with col2:
     st.title("")
     st.title("")
     st.title("")
+    st.title("")
     t = st.time_input('Enter a time of the upcoming date.')
-    print(t)
-#print(d)
-# "with" notation
-with st.sidebar:
-    st.title("Countdown")
 
+    
+
+countdown = st.empty()
+combine_time_future = datetime.combine(d,t)
+while True:
+    current_time = datetime.now()
+    difference_time = combine_time_future - current_time
+    print(difference_time)
+    if difference_time <= timedelta(0):
+        break
+
+# "with" notation
 future_date = d - (date.today())
 days = future_date.days
 
